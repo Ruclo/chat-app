551d84cf635a5b84206b26937f70f7fb4c97b122
Ruclo <vavrinec.misko@gmail.com>
2024-09-17 11:10:56 +0200

    fixed ws url

--- src/main/java/pat/mat/chat/app/config/WebSocketConfig.java
+++ src/main/java/pat/mat/chat/app/config/WebSocketConfig.java
@@ -40,8 +40,8 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/ws")
-                .setAllowedOriginPatterns("*")
+        registry.addEndpoint("/api/ws")
+                .setAllowedOriginPatterns(System.getenv("FRONTEND_URL"))
                 .withSockJS();
     }
 
